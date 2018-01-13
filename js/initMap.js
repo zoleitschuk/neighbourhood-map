f2867f10013091c87616d4509a89c54a5db71776
zoleitschuk <zachary.oleitschuk@gmail.com>
2018-01-13 12:32:39 -0700

    heatmap functional

--- js/initMap.js
+++ js/initMap.js
@@ -20,15 +20,61 @@ var findCenter = function() {
 }
 
 var findZoom = function() {
-    let zoom = 10;
+    let zoom = 13;
     return zoom;
 }
 
+var getHeatMapData = function() {
+    let heatMapData = [];
+
+    foursquareParams = {
+        mode: 'url',
+        ne: '51.149633,-113.923759',
+        sw: '50.901301,-114.310341',
+        query: 'coffee',
+        client_id: '53YSEWRJUR3R0MVJ4AB1Y54Y1UEBQLYVWIVMVQC1PN2G2M3A',
+        client_secret: 'BT2FCLFH0QBC1S20UU3L1NI1SFL04RJ1SWQ4WBXJFLWG432I',
+        version: '20180113',
+    }
+    
+    let endPoint = 'https://api.foursquare.com/v2/venues/explore?' +
+        'mode=' + foursquareParams.mode +
+        '&ne=' + foursquareParams.ne +
+        '&sw=' + foursquareParams.sw +
+        '&q=' + foursquareParams.query +
+        '&client_id=' + foursquareParams.client_id +
+        '&client_secret=' + foursquareParams.client_secret +
+        '&v=' + foursquareParams.version;
+    
+    $.getJSON(endPoint, function(data) {
+        data.response.groups[0].items.forEach(item => {
+            let hotSpotLat = item.venue.location.lat;
+            let hotSpotLng = item.venue.location.lng;
+            let rating = item.venue.rating || 1;
+            let hotSpot = {
+                location: new google.maps.LatLng(hotSpotLat, hotSpotLng),
+                weight: rating / 5,
+            };
+    
+            heatMapData.push(hotSpot);
+        });
+    });
+    return heatMapData;
+}
+
+var map, heatmap;
+
 var initMap = function() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: findZoom(),
         center: findCenter()
     });
+    heatmap = new google.maps.visualization.HeatmapLayer({
+        data: getHeatMapData(),
+        map: map
+    });
+    console.log(heatmap.data);
 
     var infoWindow = new google.maps.InfoWindow;
 
@@ -51,3 +97,6 @@ var initMap = function() {
         return marker;
     });
 }
+function toggleHeatmap() {
+    heatmap.setMap(heatmap.getMap() ? null : map);
+  }
